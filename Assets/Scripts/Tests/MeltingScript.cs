4df68ca42ba560db227f8c030412d8256c0c1a71
Aleksandar Georgiev <549190@student.saxion.nl>
2025-02-27 20:41:13 +0100

    MetalPouring (Progress)(Not Finished)

--- Assets/Scripts/Tests/MeltingScript.cs
+++ Assets/Scripts/Tests/MeltingScript.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class MeltingScript : MonoBehaviour
 {
     Gyroscope gyroscope;
+    Quaternion gyroscopeRotation;
     Quaternion ninetydegx = Quaternion.Euler(-90, 0, 0);
 
     [SerializeField]
-    private float pouringThreshold;
+    private float pouringAngle = 30f;
+    [SerializeField]
+    private float maxPourSpeed = 50f;
+    [SerializeField]
+    private float pourRate = 1f;
+    private float lastTiltAngle = 0f;
+    private float pourAmount = 0f;
+
+    private bool isPouring = false;
 
     void Start()
     {
@@ -18,9 +27,13 @@ public class MeltingScript : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(Input.gyro.attitude);
+        gyroscopeRotation = GyroToUnity(Input.gyro.attitude);
+        float zRotation = gyroscopeRotation.eulerAngles.z;
+        float yRotation = gyroscopeRotation.eulerAngles.y;
+        float xRotation = gyroscopeRotation.eulerAngles.x;
+        transform.rotation = ninetydegx * Quaternion.Euler(xRotation, yRotation, zRotation);
 
-        transform.rotation = ninetydegx * GyroToUnity(Input.gyro.attitude);
+        CheckPouring(transform.rotation);
     }
 
     private static Quaternion GyroToUnity(Quaternion q)
@@ -28,8 +41,45 @@ public class MeltingScript : MonoBehaviour
         return new Quaternion(q.x, -q.y, q.z, -q.w);
     }
 
-    private void CheckPouring()
+    private void CheckPouring(Quaternion rotation)
     {
+        Vector3 euler = rotation.eulerAngles;
+        float tiltAngle = NormalizeAngle(euler.z); // Normalize to -180 to 180
 
+        // Check if tilt is beyond the pouring threshold
+        if (tiltAngle > pouringAngle)
+        {
+            if (!isPouring)
+            {
+                isPouring = true;
+                Debug.Log("Started Pouring!");
+            }
+
+            // Calculate pour speed (change in angle per second)
+            float pourSpeed = Mathf.Abs(tiltAngle - lastTiltAngle) / Time.deltaTime;
+
+            if (pourSpeed < maxPourSpeed)
+            {
+                pourAmount += pourRate * Time.deltaTime;
+                Debug.Log("Pouring Metal: " + pourAmount);
+
+            }
+            else
+            {
+                Debug.Log("Pouring too fast! Quality decreased.");
+            }
+        }
+        else if (isPouring)
+        {
+            isPouring = false;
+            Debug.Log("Stopped Pouring.");
+        }
+
+        lastTiltAngle = tiltAngle;
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        return (angle > 180) ? angle - 360 : angle;
     }
 }
