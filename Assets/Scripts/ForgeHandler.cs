ca4f6693d841790d0e3b53a0e25396da2fd22f49
Kevin <550031@student.saxion.nl>
2025-02-28 10:58:02 +0100

    Added UserSettings to .gitignore and microphone changes

--- Assets/Scripts/ForgeHandler.cs
+++ Assets/Scripts/ForgeHandler.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ForgeHandler : MonoBehaviour
@@ -11,16 +12,37 @@ public class ForgeHandler : MonoBehaviour
     [SerializeField] private float maxScore = 5;
 
     [SerializeField] private ForgeProgressDisplay progressDisplay;
+    [SerializeField] private TextMeshProUGUI timerDisplay;
+    [SerializeField] private TextMeshProUGUI finalScore;
+
+    private bool isTimerStarted = false;
+    [SerializeField] private float allowedTime = 5;
+    private float timer = 0;
 
     // Update is called once per frame
     void Update()
     {
         float micVolume = blowDetector.RMSVolumeValue;
-        if (micVolume > microphoneThreshold) fireIntensity += Time.deltaTime;
-        else fireIntensity = Mathf.Max(0, fireIntensity - Time.deltaTime);
-        score += Time.deltaTime * ((micVolume > microphoneThreshold) ? increaseAmount : -decreaseAmount);
-        score = Mathf.Clamp(score, 0, maxScore);
-        Debug.Log($"intensity: {fireIntensity}, score: {score}");
-        progressDisplay.SetNeedleProgress(score / maxScore);
+        if (isTimerStarted)
+        {
+            if (micVolume > microphoneThreshold) fireIntensity += Time.deltaTime;
+            else fireIntensity = Mathf.Max(0, fireIntensity - Time.deltaTime);
+            score += Time.deltaTime * ((micVolume > microphoneThreshold) ? increaseAmount : -decreaseAmount);
+            score = Mathf.Clamp(score, 0, maxScore);
+            Debug.Log($"intensity: {fireIntensity}, score: {score}");
+            progressDisplay.SetNeedleProgress(score / maxScore);
+
+            timerDisplay.text = timer + "s of " + allowedTime + "s";
+        }
+
+        if (!isTimerStarted && micVolume > microphoneThreshold) isTimerStarted = true;
+        if (isTimerStarted && timer < allowedTime) timer += Time.deltaTime;
+        else if (timer >= allowedTime) DisplayFinalScore();
+    }
+
+    void DisplayFinalScore()
+    {
+        isTimerStarted = false;
+        finalScore.text = "Score: " + Mathf.RoundToInt(score);
     }
 }
