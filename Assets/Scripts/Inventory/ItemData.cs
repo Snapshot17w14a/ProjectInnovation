8bb6b8160453a9f5ab7570d6e16ae533e5de5d84
Kevin <550031@student.saxion.nl>
2025-03-03 10:23:35 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -0,0 +1,13 @@
+﻿public struct ItemData
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public ItemStats Stats { get; set; }
+
+    public ItemData(string name, int id, ItemStats stats)
+    {
+        Name = name;
+        Id = id;
+        Stats = stats;
+    }
+}
