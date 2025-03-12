763b8c42e6d503716b5958596bfd203a01e492ae
Aleksandar <549190@student.saxion.nl>
2025-03-12 16:03:24 +0100

    progress save

--- Assets/Scripts/Tests/PouringMetal.cs
+++ Assets/Scripts/Tests/PouringMetal.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.VFX;
 
 public class LiquidDropContainer : MonoBehaviour
 {
@@ -19,6 +21,7 @@ public class PouringMetal : CraftingProcess, ICraftingProcess
     [SerializeField] private float totalLiquidAmount = 100f;
     [SerializeField] private float accelerometerSpeed = 5f;
 
+    private float angleOffset = 180;
     private float currentPourSpeed = 0f;
     private float yOffest = 0.02f;
     private float currentLiquidAmount;
@@ -40,7 +43,8 @@ public class PouringMetal : CraftingProcess, ICraftingProcess
     [SerializeField] private TMP_Text amountText;
     [SerializeField] private GameObject liquidCubes;
     [SerializeField] private Transform metalParent;
-
+    [SerializeField] private VisualEffect moltenMetalVFX;
+    [SerializeField] private RectTransform vial;
 
     private Weapon weapon;
 
@@ -52,6 +56,11 @@ public class PouringMetal : CraftingProcess, ICraftingProcess
     {
         amountText.text = $" {currentLiquidAmount.ToString("F1")}";
         currentLiquidAmount = totalLiquidAmount;
+
+        Quaternion adjustedRotation = vial.rotation;
+        adjustedRotation *= Quaternion.Euler(0, 0, angleOffset); // Rotate by angleOffset on the Z axis
+        Instantiate(moltenMetalVFX, vial.position, adjustedRotation, vial);
+        moltenMetalVFX.Stop();
     }
 
     private void Update()
@@ -62,13 +71,18 @@ public class PouringMetal : CraftingProcess, ICraftingProcess
 
     private void PourMetal()
     {
+
         if (isPouring)
         {
             currentPourSpeed = Mathf.Min(currentPourSpeed + pourAcceleration * Time.deltaTime, maxPourSpeed);
+
+            EnableVFX();
         }
         else
         {
             currentPourSpeed = Mathf.Max(currentPourSpeed - pourDeceleration * Time.deltaTime, 0);
+
+            DisableVFX();
         }
 
         if(currentLiquidAmount > 0 && currentPourSpeed > 0)
@@ -77,7 +91,6 @@ public class PouringMetal : CraftingProcess, ICraftingProcess
             currentLiquidAmount = Mathf.Max(currentLiquidAmount - pouringAdjusted, 0);
             amountText.text = $"{currentLiquidAmount.ToString("F1")}";
             Instantiate(liquidCubes, new Vector3(transform.position.x, transform.position.y - yOffest, transform.position.z), Quaternion.identity, metalParent).AddComponent<LiquidDropContainer>().Amount = pouringAdjusted;
-            
         }
         else if(currentLiquidAmount <= 0) IsPouringFinished(currentLiquidAmount);
     }
@@ -115,6 +128,16 @@ public class PouringMetal : CraftingProcess, ICraftingProcess
     public void StartProcess(ref Weapon item)
     {
         weapon = item;
+    }
+
+    private void EnableVFX()
+    {
+        moltenMetalVFX.Play();
+    }
+
+    private void DisableVFX()
+    {
+        moltenMetalVFX.Stop();
     }
 }
 
