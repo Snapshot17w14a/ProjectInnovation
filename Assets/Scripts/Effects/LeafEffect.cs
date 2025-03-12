b2c590b7763a3ec0225a5c4f4c235908a1ecf6ed
Aleksandar <549190@student.saxion.nl>
2025-03-12 14:10:11 +0100

    progress on decoration vfx

---
+++ Assets/Scripts/Effects/LeafEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafEffect : IWeaponEffect
+{
+    public void ApplyEffect(Character target, params object[] parameters)
+    {
+        throw new System.NotImplementedException();
+    }
+}
