e76f880d78bf6bc2a67e2cb669183235a5977777
JimmevD <72076148+JimmevD@users.noreply.github.com>
2025-03-13 20:02:03 +0100

    Battlescene UI changes

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,9 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class LevelSelector : MonoBehaviour
 {
     private BattleManager battleManager;
     [SerializeField] private UIManager uiManager;
+    [SerializeField] private TextMeshProUGUI levelNumber;
 
     void Start()
     {
@@ -13,6 +15,7 @@ public class LevelSelector : MonoBehaviour
     public void SetBattleContainer(Battle container)
     {
         battleManager.SetBattleContainer(container);
+        levelNumber.text = container.name;
         uiManager.CloseAllOverlays();
     }
 }
